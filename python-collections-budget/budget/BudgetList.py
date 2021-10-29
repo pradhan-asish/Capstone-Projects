6b98811c860307a07b6cdd0ff07988b7bf1219e8
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-29 18:27:54 +0530

    Constructor created

--- python-collections-budget/budget/BudgetList.py
+++ python-collections-budget/budget/BudgetList.py
@@ -1,2 +1,8 @@
 class BudgetList():
-    pass
+    def __init__(self,budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_averages = 0
+        self.overages = []
+    
