08c8af8d94e6bec1b3509d4dae409e61ce47558f
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-29 23:21:16 +0530

    Modification done in method

--- python-collections-budget/budget/BudgetList.py
+++ python-collections-budget/budget/BudgetList.py
@@ -3,9 +3,11 @@ class BudgetList():
         self.budget = budget
         self.sum_expenses = 0
         self.expenses = []
-        self.sum_averages = 0
+        self.sum_overages = 0
         self.overages = []
     def append(self,item):
         if self.sum_expenses + item < self.budget :
             self.sum_expenses = self.sum_expenses + item
-            append(item)
+            self.expenses.append(item)
+        else :
+            
