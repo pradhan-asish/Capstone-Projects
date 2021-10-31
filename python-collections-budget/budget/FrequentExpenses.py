ecb328449de2fcc09ff79dad55d06b79eda3e88c
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-31 23:28:09 +0530

    spending_counter created

--- python-collections-budget/budget/FrequentExpenses.py
+++ python-collections-budget/budget/FrequentExpenses.py
@@ -1,9 +1,9 @@
 import Expense
 import collections
 
-spending_counter = collections.Counter()
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
+spending_counter = collections.Counter()
