621e64d19fb2768a4e91d8b178fe7ac658c34ed9
pradhan-asish <pradhan.asish1994@gmail.com>
2021-10-31 23:31:02 +0530

    TOp 5 categories created

--- python-collections-budget/budget/FrequentExpenses.py
+++ python-collections-budget/budget/FrequentExpenses.py
@@ -8,3 +8,5 @@ for expense in expenses.list:
     spending_categories.append(expense.category)
 spending_counter = collections.Counter(spending_categories)
 print(spending_counter)
+top5 = spending_counter.most_common(5)
+print(top5)
