f7e596282fc0da93fb8385a095ddaa6980163967
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-01 23:19:31 +0530

    if condition applied

--- python-collections-budget/budget/Expense.py
+++ python-collections-budget/budget/Expense.py
@@ -42,4 +42,7 @@ class Expenses():
 
             return [necessary_expenses, food_expenses, unnecessary_expenses]
     def categorize_set_comprehension(self):
-        pass
+            necessary_expenses = {}
+            for x in self.list:
+                if (x.category == 'Phone' | x.category == 'Auto and Gas' | x.category == 'Utilities' | x.category == 'Mortgage' ):
+                    pass
