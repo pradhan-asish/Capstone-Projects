22404b8dd9847646a221ca198d63d37fcd1a4032
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-02 17:18:50 +0530

    Divide set comprehension called

--- python-collections-budget/budget/Expense.py
+++ python-collections-budget/budget/Expense.py
@@ -49,3 +49,4 @@ class Expenses():
             food_expenses = {x for x in self.list  if (x.category == 'Groceries' | x.category == 'Eating Out' ) }
             unnecessary_expenses.set(self.list)-necessary_expenses-food_expenses
             return {necessary_expenses,food_expenses,unnecessary_expenses}
+    
