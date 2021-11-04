c8bed0cb3c69c7b6fd0601594b836ff1d7f663a0
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-04 22:10:28 +0530

    Return list

--- python-decoding-sensor-data/sensor/sensor_app.py
+++ python-decoding-sensor-data/sensor/sensor_app.py
@@ -7,6 +7,8 @@ from load_data import load_sensor_data
 data = []
 print("Sensor Data App")
 data = load_sensor_data()
+print("Loaded records: {}",format(len(data)))
+
 ##############################
 
 # Module 1 code here:
