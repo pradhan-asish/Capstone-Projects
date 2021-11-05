9095bcbae1825c17531624fac1a8e98a00e00638
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-05 17:50:39 +0530

    return statement added

--- python-decoding-sensor-data/sensor/house_info.py
+++ python-decoding-sensor-data/sensor/house_info.py
@@ -17,4 +17,4 @@ class HouseInfo():
         for record in self.data:
             if(record['date'] == rec_date.strftime("%m%d%y")) :
                 field_data.append(record[field])
-        
+        return field_data
