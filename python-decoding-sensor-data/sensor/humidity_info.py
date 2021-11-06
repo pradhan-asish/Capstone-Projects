ab2e004f71840c2616f8087f5636ff28efc5cb46
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-06 19:56:10 +0530

    return statement added

--- python-decoding-sensor-data/sensor/humidity_info.py
+++ python-decoding-sensor-data/sensor/humidity_info.py
@@ -6,9 +6,10 @@ class HumidityData(HouseInfo):
         recs = []
         for rec in data:
             recs.append(float(rec)*100)
+        return recs
     def get_data_by_area(self,rec_area=0):
         recs = super().get_data_by_area("humidity",rec_area)
-        return self._convert_data(recs)
+        return self._conver_data(recs)
     def get_data_by_data(self,rec_date=date.today()):
         recs = super().get_data_by_date("humidity",rec_date)
-        return self._convert_data(recs)
+        return self._conver_data(recs)
