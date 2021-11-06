c7805412ce85d7722f828e213d2fc73ade1fc2b0
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-06 09:03:16 +0530

    _conver_data private method created

--- python-decoding-sensor-data/sensor/humidity_info.py
+++ python-decoding-sensor-data/sensor/humidity_info.py
@@ -1,4 +1,7 @@
+from house_info import HouseInfo
 
 class HumidityData(HouseInfo):
-    _conver_data(self,data):
+    def _conver_data(self,data):
         recs = []
+        for rec in data:
+            recs.append(float(rec)*100)
