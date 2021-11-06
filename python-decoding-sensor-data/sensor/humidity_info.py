290b62d5d0bc2141de29e438ca45021eecfdfe9a
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-06 19:21:37 +0530

    get_data_by_data method created

--- python-decoding-sensor-data/sensor/humidity_info.py
+++ python-decoding-sensor-data/sensor/humidity_info.py
@@ -8,3 +8,6 @@ class HumidityData(HouseInfo):
     def get_data_by_area(self,rec_area=0):
         recs = super().get_data_by_area("humidity",rec_area)
         return self._convert_data(recs)
+    def get_data_by_data(self,rec_date=date.today()):
+        recs = super().get_data_by_date("humidity",rec_date)
+        return self._convert_data(recs)
