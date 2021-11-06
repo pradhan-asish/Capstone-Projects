e6e766fd927f8c3f1475b854d2ffcba84f2be4fb
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-06 08:46:09 +0530

    file recirifed

--- python-decoding-sensor-data/sensor/temperature_info.py
+++ python-decoding-sensor-data/sensor/temperature_info.py
@@ -8,9 +8,8 @@ class TemperatureData(HouseInfo):
             recs.append(int(rec,base=10))
         return recs
     def get_data_by_area(self,rec_area=0):
-        recs = super.get_data_by_area("temperature",rec_area)
-        return _convert_data(recs)
+        recs = super().get_data_by_area("temperature",rec_area)
+        return self._convert_data(recs)
     def get_data_by_data(self,rec_date=date.today()):
-        recs = super.get_data_by_date("temperature",rec_date)
-        return _convert_data(recs)
-    
+        recs = super().get_data_by_date("temperature",rec_date)
+        return self._convert_data(recs)
