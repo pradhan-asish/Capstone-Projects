d216ccf65ea587446b567378225508d70baf7337
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-06 19:20:42 +0530

    get_data_by_area mehod created

--- python-decoding-sensor-data/sensor/humidity_info.py
+++ python-decoding-sensor-data/sensor/humidity_info.py
@@ -5,3 +5,6 @@ class HumidityData(HouseInfo):
         recs = []
         for rec in data:
             recs.append(float(rec)*100)
+    def get_data_by_area(self,rec_area=0):
+        recs = super().get_data_by_area("humidity",rec_area)
+        return self._convert_data(recs)
