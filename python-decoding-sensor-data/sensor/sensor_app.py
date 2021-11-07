d930f64f729a41f6df9a083519368aedb9824c58
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-07 17:55:33 +0530

    particle_data variable created

--- python-decoding-sensor-data/sensor/sensor_app.py
+++ python-decoding-sensor-data/sensor/sensor_app.py
@@ -6,6 +6,7 @@ from datetime import date
 from datetime import datetime
 from temperature_info import TemperatureData
 from humidity_info import HumidityData
+from particle_count_info import ParticleData
 from statistics import mean
 ##############################
 # Do not remove these two lines
@@ -14,6 +15,8 @@ data = []
 print("Sensor Data App")
 data = load_sensor_data()
 print("Loaded records: {}",format(len(data)))
+
+# Module 1 code here:
 house_info = HouseInfo(data)
 test_area=1
 recs = house_info.get_data_by_area("id",test_area)
@@ -21,25 +24,19 @@ print("\nHouse sensor records for area {} = {}".format(test_area,len(recs)))
 test_date = datetime.strptime("5/9/20","%m/%d/%y")
 recs = house_info.get_data_by_date("id",test_date)
 print("\nHouse sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"),len(recs)))
+# Module 2 code here:
 temperature_data = TemperatureData(data)
 recs = temperature_data.get_data_by_area(test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area,len(recs)))
 #print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs),min(recs)))
 ##print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"),len(recs)))
 #print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs),min(recs)))
-
+# Module 3 code here:
 humidity_data = HumidityData(data)
 recs = humidity_data.get_data_by_area(test_area)
 print("\nHouse Humidity sensor records for area {} = {}".format(test_area,len(recs)))
 print("\tAverage: {} humidity".format(mean(recs)))
-##############################
-
-# Module 1 code here:
-
-# Module 2 code here:
-
-# Module 3 code here:
-
 # Module 4 code here:
+particle_data=ParticleData(data)
 
 # Module 5 code here:
