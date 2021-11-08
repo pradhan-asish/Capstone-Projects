c61516b658f356d7ae2685071ba11db8d28ba77c
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-08 21:57:35 +0530

    game_frame variable assigned

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/data.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/data.py
@@ -7,3 +7,7 @@ game_files = glob.glob(os.path.join(os.getcwd(),'games','*.EVE'))
 print(os.path.join(os.getcwd(),'games','*.EVE'))
 print(game_files)
 game_files.sort()
+
+for game_file in game_files:
+    game_frame = pd.read_csv(game_file,names = ['type','multi2','multi3','multi4','multi5','multi6','event'])
+    
