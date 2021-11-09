80d6ffd1be22c3a488a45898f11f4105aefe32a2
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-09 19:24:55 +0530

    strike_outs varaible created

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/pitching.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/pitching.py
@@ -6,5 +6,5 @@ from data import games
 plays = games.loc[(games['type'] == 'play')]
 print(plays)
 strike_outs = games[games['event'].str.contains('K')]
+strike_outs=strike_outs.groupby(['year','game_id']).size()
 print(strike_outs)
-strike_outs = strike_outs.groupby(['year','game_id'])
