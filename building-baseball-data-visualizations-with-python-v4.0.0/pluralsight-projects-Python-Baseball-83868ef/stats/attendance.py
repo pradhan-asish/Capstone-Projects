48ddef502e2918742a43635d748a788be89cb5f4
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-09 13:22:55 +0530

    atendence variable created

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/attendance.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/attendance.py
@@ -3,3 +3,5 @@ import matplotlib.pyplot as py
 from data import games
 
 print(games)
+
+atendence = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendence'),['type','multi2','year','multi3']]
