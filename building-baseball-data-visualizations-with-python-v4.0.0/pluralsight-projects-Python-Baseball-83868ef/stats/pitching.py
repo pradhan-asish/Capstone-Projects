9fdd0b5483c1e35f63f08fd899ebf1eb15732433
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-09 19:06:41 +0530

    plays variable modified

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/pitching.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/pitching.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 
 from data import games
 
-plays = games.loc[(games['type'] == 'play'),['type']]
-
+plays = games.loc[(games['type'] == 'play')]
 print(plays)
+strike_outs = plays[plays['type'].str.contains('K')]
+print(strike_outs)
