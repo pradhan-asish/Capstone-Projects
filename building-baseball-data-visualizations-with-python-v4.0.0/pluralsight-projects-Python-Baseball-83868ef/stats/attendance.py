81e0458f5018676017219dbf13e769d195128d14
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-09 13:23:40 +0530

    info created

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/attendance.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/attendance.py
@@ -5,3 +5,4 @@ from data import games
 print(games)
 
 atendence = games.loc[(games['type'] == 'info') & (games['multi2'] == 'attendence'),['type','multi2','year','multi3']]
+print(atendence.info())
