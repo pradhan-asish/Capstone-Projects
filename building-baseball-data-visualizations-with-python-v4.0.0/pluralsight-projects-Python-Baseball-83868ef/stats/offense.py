cb8f16914d53bdd55e2470e70d522c3c831340dc
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-10 18:27:27 +0530

    plays dataframe columns modiifed

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
@@ -4,3 +4,5 @@ from data import games
 
 plays = games.loc[(games['type'] == 'play')]
 print(plays)
+plays.columns = ['year','inning','team','player','count','pitches','event','game_id','year']
+print(plays.describe())
