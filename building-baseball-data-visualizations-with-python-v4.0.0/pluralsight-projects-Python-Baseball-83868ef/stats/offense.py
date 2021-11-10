c864c4a5c851e05388e8b173089d594894618f98
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-10 22:18:38 +0530

    contains keyword used

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
@@ -8,4 +8,4 @@ print(plays)
 plays.columns = ['year','inning','team','player','count','pitches','event','game_id','year']
 print(plays.describe())
 
-hits = plays.loc[re.search(r"^(?:S(?!B)|D|T|HR)",plays['event']),['inning','event']]
+hits = plays.loc[plays['event'].contains(re.search(r"^(?:S(?!B)|D|T|HR)",plays['event'])),['inning','event']]
