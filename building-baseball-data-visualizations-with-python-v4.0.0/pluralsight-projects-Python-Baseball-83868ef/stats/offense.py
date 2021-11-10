22e59eec23656859b4150847683e836cc23e47f1
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-10 21:44:21 +0530

    regex is not working

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from data import games
+import re
 
 plays = games.loc[(games['type'] == 'play')]
 print(plays)
 plays.columns = ['year','inning','team','player','count','pitches','event','game_id','year']
 print(plays.describe())
+
+hits = plays.loc[re.search(r"^(?:S(?!B)|D|T|HR)",plays['event']),['inning','event']]
