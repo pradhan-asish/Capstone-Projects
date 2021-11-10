b7f3f5861082830afd46be736f35fce06c8803ea
pradhan-asish <pradhan.asish1994@gmail.com>
2021-11-10 23:50:49 +0530

    hits reset index created

--- building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
+++ building-baseball-data-visualizations-with-python-v4.0.0/pluralsight-projects-Python-Baseball-83868ef/stats/offense.py
@@ -17,3 +17,4 @@ replacements = {r'^S(.*)': 'single',r'^D(.*)': 'double',r'^T(.*)': 'triple',r'^H
 hit_type = hits['event'].replace(replacements,regex=True)
 
 hits=hits.groupby(['inning','event']).size()
+hits = hits.reset_index(name = 'count')
